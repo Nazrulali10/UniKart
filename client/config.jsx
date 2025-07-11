19462f943800651de2efd2a263f948ba91bd0ccb
Nazrul Ali.N <nazrulali555@gmail.com>
2025-07-11 16:53:27 +0530

    meta 2

--- client/config.jsx
+++ client/config.jsx
@@ -1,7 +1,6 @@
-import { meta } from "@eslint/js";
 
 const BASE_URL =
-  meta.env.VITE_ENV === 'development'
+  process.env.NODE_ENV === 'development'
     ? 'http://localhost:4000/api'
     : `${meta.env.VITE_BACKEND_URL}/api`;
 
