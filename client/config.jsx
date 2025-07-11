bb93bf899f6f235a20b5dd02d9257eb21a5a1d20
Nazrul Ali.N <nazrulali555@gmail.com>
2025-07-11 16:17:35 +0530

    config.js

--- client/config.jsx
+++ client/config.jsx
@@ -2,6 +2,6 @@
 const BASE_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:4000/api'
-    : process.env.BACKEND_URL;
+    : `${process.env.BACKEND_URL}/api`;
 
 export default BASE_URL;
